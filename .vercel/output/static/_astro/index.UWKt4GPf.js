d54070edef2aa64b088e9bc09d684520fc11b930
Balavignarajan-Synamen <balavignarajan.p@synamen.com>
2025-06-18 16:56:05 +0530

    aos added

---
+++ .vercel/output/static/_astro/index.UWKt4GPf.js
@@ -0,0 +1,1 @@
+import{G as r}from"./iconBase.Rsx8IN41.js";function o(t){return r({attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0V0z"},child:[]},{tag:"path",attr:{d:"M8.59 16.59 13.17 12 8.59 7.41 10 6l6 6-6 6-1.41-1.41z"},child:[]}]})(t)}export{o as M};
